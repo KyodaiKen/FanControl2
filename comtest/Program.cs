3196f45a9a56250d07afd1cfa26bf22b95d77f49
insomnyawolf <insomnyawolf@gmail.com>
2021-10-25 12:57:42 +0200

    ProtocolIdea

--- comtest/Program.cs
+++ comtest/Program.cs
@@ -1,16 +1,47 @@
-﻿using System;
-using System.IO;
-
-
-
+﻿using FanController;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace comtest
 {
     class Program
     {
-        static void Main(string[] args)
+        static List<Controller> controllers;
+        static async Task Main(string[] args)
         {
-            FanContManager manager = new FanContManager();
+            controllers = await ControllerFactory.GetCompatibleDevicesAsync();
+
+            foreach (var device in controllers)
+            {
+                device.StatusUpdated += Controller_StatusUpdated;
+                device.SettingsUpdated += Device_SettingsUpdated;
+                device.StartListening();
+            }
+
+            Console.CancelKeyPress += Console_CancelKeyPress;
+
+            await Task.Delay(-1);
+        }
+
+        private static void Device_SettingsUpdated(string DeviceId, string Data)
+        {
+            Console.WriteLine($"Settings Updated For => '{DeviceId}' => '{Data}'");
+        }
+
+        private static void Controller_StatusUpdated(string DeviceId, string Data)
+        {
+            Console.WriteLine($"Device => '{DeviceId}' Data => '{Data}'");
+        }
+
+        private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            foreach (var item in controllers)
+            {
+                item.StopListening();
+            }
+
+            Environment.Exit(0);
         }
     }
 }
